2e0f4bd750b49668bf44399cafb9cfbcc1d0361b
arjunlaxman2003 <153803923+arjunlaxman2003@users.noreply.github.com>
2024-04-28 19:38:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,93 @@
-// Dimensions and SVG setup
+// Initial configurations
 const width = 960, height = 600;
+const colorScheme = d3.schemeReds[6];
+const colorScale = d3.scaleQuantize().range(colorScheme);
+const path = d3.geoPath();
+
+// Append SVG to the map container
 const svg = d3.select("#map").append("svg")
     .attr("width", width)
     .attr("height", height);
-const tooltip = d3.select("body").append("div")
-    .attr("class", "tooltip")
-    .style("opacity", 0);
 
-let projection = d3.geoAlbersUsa().scale(1300).translate([width / 2, height / 2]);
-let path = d3.geoPath().projection(projection);
+// Define tooltip
+const tooltip = d3.select("body").append("div")
+    .attr("id", "tooltip")
+    .style("position", "absolute")
+    .style("visibility", "hidden")
+    .style("padding", "10px")
+    .style("background", "white")
+    .style("border", "1px solid #ccc")
+    .style("border-radius", "5px")
+    .style("pointer-events", "none");
+
+// Load geographic and data files
+Promise.all([
+    d3.json("https://d3js.org/us-10m.v1.json"), 
+    d3.csv("Data.csv")
+]).then(function (files) {
+    const us = files[0];
+    const data = files[1];
+
+    // Aggregate data by state
+    const dataMap = {};
+    data.forEach(d => {
+        dataMap[d.State] = {
+            code: stateNameToCode[d.State],
+            cases: +d.Cases,
+            deaths: +d.Deaths,
+            vaccination: +d.Doses  
+        };
+    });
+
+    // Draw initial map with default data type (cases)
+    drawMap(us, dataMap, "cases");
+
+    // Set up UI interaction
+    document.getElementById('data-select').addEventListener('change', function() {
+        drawMap(us, dataMap, this.value);
+    });
+});
+
+// Draw or update the map based on the dataset
+function drawMap(us, dataMap, dataType) {
+    const dataValues = Object.values(dataMap).map(d => d[dataType]);
+    colorScale.domain([d3.min(dataValues), d3.max(dataValues)]);
+
+    svg.selectAll("*").remove(); // Clear previous drawings
+
+    const states = svg.append("g")
+        .attr("class", "states")
+        .selectAll("path")
+        .data(topojson.feature(us, us.objects.states).features)
+        .enter().append("path")
+        .attr("fill", d => {
+            const stateData = dataMap[d.properties.name];
+            return stateData ? colorScale(stateData[dataType]) : "#ccc";
+        })
+        .attr("d", path)
+        .on("mouseover", (event, d) => {
+            tooltip.style("visibility", "visible")
+                .html(() => {
+                    const stateData = dataMap[d.properties.name];
+                    const dataValue = stateData ? stateData[dataType] : "No data";
+                    return `<strong>${d.properties.name}</strong>: ${dataValue}`;
+                })
+                .style("left", (event.pageX + 10) + "px")
+                .style("top", (event.pageY - 28) + "px");
+        })
+        .on("mousemove", (event) => {
+            tooltip.style("left", (event.pageX + 10) + "px")
+                .style("top", (event.pageY - 28) + "px");
+        })
+        .on("mouseout", () => {
+            tooltip.style("visibility", "hidden");
+        });
+
+    // Optional: Draw state borders
+    svg.append("path")
+        .attr("class", "state-borders")
+        .attr("d", path(topojson.mesh(us, us.objects.states, (a, b) => a !== b)));
+}
 
 // State codes to names mapping
 const stateCodeToName = {
@@ -27,24 +106,7 @@ const stateCodeToName = {
     "WI": "Wisconsin", "WV": "West Virginia", "WY": "Wyoming"
 };
 
-// Load data and map simultaneously
-Promise.all([
-    d3.csv("Data.csv"),
-    d3.json("https://d3js.org/us-10m.v1.json")
-]).then(([data, us]) => {
-    // Mapping of data to state names using state codes
-    const dataMap = new Map(data.map(d => [stateCodeToName[d.State], d]));
-
-    const states = topojson.feature(us, us.objects.states).features;
-
-    // Draw each state and add interactive features
-    const statesPaths = svg.selectAll(".state")
-        .data(states)
-        .enter().append("path")
-        .attr("class", "state")
-        .attr("d", path)
-        .attr("fill", d => colorScaleCases(dataMap.get(stateCodeToName[d.id])?.Cases || 0))
-        .on("mouseover", (event, d) => {
-            const stateData = dataMap.get(stateCodeToName[d.id]);
-            tooltip.style("opacity", 1)
-                .html(`State: ${stateCodeToName[d.id]}
+const stateNameToCode = {};
+Object.keys(stateCodeToName).forEach(code => {
+    stateNameToCode[stateCodeToName[code]] = code;
+});
