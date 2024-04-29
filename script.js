ebeb805dc7bfb01b26269e95e83ac9aa618d833f
arjunlaxman2003 <153803923+arjunlaxman2003@users.noreply.github.com>
2024-04-28 21:03:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,20 +5,9 @@ const colorScale = d3.scaleQuantize().range(colorScheme);
 const path = d3.geoPath();
 
 // Append SVG to the map container
-const svg = d3.select("#map").append("svg")
+var svg = d3.select("svg")
     .attr("width", width)
     .attr("height", height);
-
-// Define tooltip
-const tooltip = d3.select("body").append("div")
-    .attr("id", "tooltip")
-    .style("position", "absolute")
-    .style("visibility", "hidden")
-    .style("padding", "10px")
-    .style("background", "white")
-    .style("border", "1px solid #ccc")
-    .style("border-radius", "5px")
-    .style("pointer-events", "none");
 
 // Load geographic and data files
 Promise.all([
@@ -39,13 +28,8 @@ Promise.all([
         };
     });
 
-    // Draw initial map with default data type (cases)
+    // Draw map with default data type (cases)
     drawMap(us, dataMap, "cases");
-
-    // Set up UI interaction
-    document.getElementById('data-select').addEventListener('change', function() {
-        drawMap(us, dataMap, this.value);
-    });
 });
 
 // Draw or update the map based on the dataset
@@ -53,13 +37,12 @@ function drawMap(us, dataMap, dataType) {
     const dataValues = Object.values(dataMap).map(d => d[dataType]);
     colorScale.domain([d3.min(dataValues), d3.max(dataValues)]);
 
-    svg.selectAll("*").remove(); // Clear previous drawings
+    svg.selectAll(".state").remove(); // Clear previous state paths
 
-    const states = svg.append("g")
-        .attr("class", "states")
-        .selectAll("path")
+    svg.selectAll(".state")
         .data(topojson.feature(us, us.objects.states).features)
         .enter().append("path")
+        .attr("class", "state")
         .attr("fill", d => {
             const stateCode = d.properties.name;
             const stateData = dataMap[stateCode];
@@ -67,13 +50,12 @@ function drawMap(us, dataMap, dataType) {
         })
         .attr("d", path)
         .on("mouseover", (event, d) => {
-    const stateCode = d.properties.name;
-    const stateData = dataMap[stateCode];
-    const stateName = stateData ? stateData.state : stateCode;
-    const dataValue = stateData ? stateData[dataType] : "No data";
-    showTooltip(event.pageX, event.pageY, stateName, stateCode, dataValue);
-})
-
+            const stateCode = d.properties.name;
+            const stateData = dataMap[stateCode];
+            const stateName = stateData ? stateData.state : stateCode;
+            const dataValue = stateData ? stateData[dataType] : "No data";
+            showTooltip(event.pageX, event.pageY, stateName, stateCode, dataValue);
+        })
         .on("mousemove", (event) => {
             moveTooltip(event.pageX, event.pageY);
         })
@@ -81,24 +63,28 @@ function drawMap(us, dataMap, dataType) {
             hideTooltip();
         });
 
-    // Optional: Draw state borders
+    // Draw state borders
     svg.append("path")
         .attr("class", "state-borders")
-        .attr("d", path(topojson.mesh(us, us.objects.states, (a, b) => a !== b)));
+        .datum(topojson.mesh(us, us.objects.states, (a, b) => a !== b))
+        .attr("d", path);
 }
 
 function showTooltip(x, y, stateName, stateCode, dataValue) {
-    tooltip.style("visibility", "visible")
+    d3.select("#tooltip")
+        .style("visibility", "visible")
         .html(`<strong>${stateName}</strong> (${stateCode}): ${dataValue}`)
         .style("left", (x + 10) + "px")
         .style("top", (y - 28) + "px");
 }
 
 function moveTooltip(x, y) {
-    tooltip.style("left", (x + 10) + "px")
+    d3.select("#tooltip")
+        .style("left", (x + 10) + "px")
         .style("top", (y - 28) + "px");
 }
 
 function hideTooltip() {
-    tooltip.style("visibility", "hidden");
+    d3.select("#tooltip")
+        .style("visibility", "hidden");
 }
